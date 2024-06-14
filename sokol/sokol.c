d9aaf53a6a6573d214cdfb13c1772ff5add03d15
vkayy <vkrice2475@gmail.com>
2024-06-14 13:47:43 +0100

    refactor: remove fruit and score functionality from template

---
+++ sokol/sokol.c
@@ -0,0 +1,17 @@
+#define SOKOL_IMPL
+#if defined(_MSC_VER)
+#define SOKOL_D3D11
+#elif defined(__EMSCRIPTEN__)
+#define SOKOL_GLES3
+#elif defined(__APPLE__)
+// NOTE: on macOS, sokol.c is compiled explicitly as ObjC
+//#define SOKOL_GLCORE
+#define SOKOL_METAL
+#else
+#define SOKOL_GLCORE
+#endif
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+#include "sokol_audio.h"
+#include "sokol_glue.h"
+#include "sokol_log.h"
